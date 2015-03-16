33d37a32c59d6b8f2ca1061a83da37d24ff4ea0c
DarrenMoody <moodytoons@gmail.com>
2015-03-15 22:26:11 -0600

    Added jumping to ball and props to the scene.
    After adding the ability for the 'player' ball to jump, I was then
    challenged to figure out how to stop the ability to jump over and over
    again, essentially becoming like a 3D version of the smartphone app
    "Flappy Bird". I stopped this by coming up with a way to know whether or
    not the ball is in the air or on the ground. So using boolean values I
    made it so the ball can only jump if it's on the ground. Fun stuff!

--- Roll-a-ball/Assets/Scripts/PlayerController.cs
+++ Roll-a-ball/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,23 @@ using System.Collections;
 public class PlayerController : MonoBehaviour 
 {
 	public float speed;
+	public float jump;
+	private bool inAir = false;
+	public float playerYPos = 0.50f;
+
+	void Update() {
+		if(Input.GetKeyDown (KeyCode.Space)) { // ADDS A JUMP ON SPACEBAR KEYDOWN
+			if (inAir == false) {
+				this.rigidbody.AddForce(new Vector3(0, jump, 0));
+			}
+		}
+		if(this.rigidbody.position.y > playerYPos) { // CHECKS IF PLAYER IS IN THE AIR
+			inAir = true;	
+			Debug.Log(this.rigidbody.position.y);
+		}
+		if (this.rigidbody.position.y <= playerYPos) 
+			inAir = false;
+	}
 
 	void FixedUpdate()
 	{
@@ -15,9 +32,9 @@ public class PlayerController : MonoBehaviour
 		rigidbody.AddForce (movement * speed * Time.deltaTime);
 	}
 
-	void OnTriggerEnter(Collider other) 
+	void OnTriggerEnter(Collider other)
 	{
-		if (other.gameObject.tag == "Pickup") 
+		if(other.gameObject.tag == "Pickup") 
 		{
 			other.gameObject.SetActive(false);
 		}
